e2765a67de223b82c81fbb27e2bbcfc71d291f03
Toad114514 <xiaolan2332021@163.com>
2024-10-17 13:46:47 +0800

    [setup.sh] add version and some package install

--- setup.sh
+++ setup.sh
@@ -3,11 +3,11 @@ echo "欢迎安装 Staxle!"
 echo "接下来将开始安装 Staxle"
 echo "===============Staxle Info==================="
 echo "Termux 的懒人安装和管理脚本工具，快速让你上手 Termux"
-echo "准备安装的版本为："
+echo "准备安装的版本为：1.03.6"
 echo "===============Start Install==================="
 sleep 3
 echo "[INFO] 安装依赖..."
-pkg install python git -y
+pkg install python git vim openssh -y
 echo "[INFO] 安装pip依赖..."
 pip install requests pywebio tqdm
 echo "[INFO] 拉取远程仓库..."
