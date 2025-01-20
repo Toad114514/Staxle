200efb0151ef6e4d6d20f27597e3431e202e1ffb
Toad114514 <xiaolan2332021@163.com>
2025-01-21 02:33:11 +0800

    add(webpy): add desktop termux-x11 settings

--- core/web/x11.py
+++ core/web/x11.py
@@ -7,13 +7,17 @@ def insxfce4():
     out.toast("正在安装 xfce4.....(进程可在 termux 中查看)")
     os.system('pkg install xfce4 -y')
     out.popup('操作完成','你已经安装好了 xfce4')
+    with open("./core/web/xfce4") as f:
+        f.write("xfce4")
     
 def conftx11():
-    out.toast("not done!!!!!!!")
+    out.toast("正在安装Termux-x11...")
+    os.system("apt install termux-x11-nightly")
+    out.popup("操作完成","termux-x11安装好了。请在服务器上安装termux-x11，没有就去找")
     
-def startde():
+def startde(cmd):
     os.system("termux-x11 :0 &")
-    os.system("env DISPLAY=:0 startxfce4 &")
+    os.system(f"env DISPLAY=:0 {cmd} &")
     out.popup("启动完成！","现在可以打开termux-x11查看桌面环境了")
 
 def x11():
@@ -23,4 +27,5 @@ def x11():
     out.put_text("设置你的termux-x11在你的机子上")
     out.put_link("返回主页", app="index")
     
-    out.put_buttons(["安装xfce4",'配置termux-x11','启动桌面环境'], onclick=[insxfce4, conftx11, startde])
+    out.
+    out.put_buttons(["安装xfce4",'配置termux-x11','启动桌面环境'], onclick=[insxfce4, conftx11, lambda: startde("startxfce4")])
